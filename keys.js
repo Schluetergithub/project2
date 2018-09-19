f7380d6c27a00f64207c5224758883b474f1007a
jponly <jpangeli514@gmail.com>
2018-09-18 21:24:48 -0700

    env

--- keys.js
+++ keys.js
@@ -1,13 +1,12 @@
 // console.log('this is loaded');
 
-exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+exports.sportsradar = {
+    key: process.env.SPORTSRADAR_KEY,
+    
   };
   
-  exports.twitter = {
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  exports.fantasydata = {
+    key: process.env.FANTASYDATA_KEY,
+
+  
   };
