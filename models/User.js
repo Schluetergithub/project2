a2c1371536322eadefceab27437b8ae40d23897d
larry.moua <lkmoua@gmail.com>
2018-09-25 21:00:54 -0700

    updated user model js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,19 @@
 module.exports = function(sequelize, DataTypes) {
     var Users = sequelize.define("users", {
-      first_name: DataTypes.STRING,
-      last_name: DataTypes.STRING,
-      email: DataTypes.STRING,
-      user_password: DataTypes.STRING,
-      user_passconfirm: DataTypes.STRING,
-      agree: DataTypes.BOOLEAN,
 
-      team_name: DataTypes.STRING,
-      currency: DataTypes.INTEGER,
+      username: DataTypes.STRING,
+      qb: DataTypes.STRING,
+      qbYards: DataTypes.INTEGER,
+      qbTouchdowns: DataTypes.INTEGER,
+      qbScore: DataTypes.INTEGER,
+      rb: DataTypes.STRING,
+      rbYards: DataTypes.INTEGER,
+      rbTouchdowns: DataTypes.INTEGER,
+      rbScore: DataTypes.INTEGER,
+      wr: DataTypes.STRING,
+      wrYards: DataTypes.INTEGER,
+      wrTouchdowns: DataTypes.INTEGER,
+      wrScore: DataTypes.INTEGER,
 
       createdAt: DataTypes.DATE,
       updatedAt: DataTypes.DATE
